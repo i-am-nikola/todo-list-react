28781cb2c7af28e35b95da96ab2b1107fecfcfb2
Hong Dung Nguyen <hongdung6992@gmail.com>
2019-10-11 10:00:34 +0700

    Updates

---
+++ precache-manifest.31fc2dc7d5694c2e15e255621f2fc05d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7969a1a464b98ce69ce5fc56e4fdfb07",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "8b33ffe3eea4e3ff557d",
+    "url": "/todo-list-react/static/css/main.ba22bd52.chunk.css"
+  },
+  {
+    "revision": "a521618783a79445bf56",
+    "url": "/todo-list-react/static/js/2.94afa0ac.chunk.js"
+  },
+  {
+    "revision": "8b33ffe3eea4e3ff557d",
+    "url": "/todo-list-react/static/js/main.0b1b1e0e.chunk.js"
+  },
+  {
+    "revision": "cf9a49b5a7b846019517",
+    "url": "/todo-list-react/static/js/runtime~main.bf24d976.js"
+  }
+]);
